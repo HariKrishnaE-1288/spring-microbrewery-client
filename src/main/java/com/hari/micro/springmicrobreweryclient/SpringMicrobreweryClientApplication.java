58e99224d9f8d051a2fd43b31c274127efea731e
HariKrishnaE-1288 <56486503+HariKrishnaE-1288@users.noreply.github.com>
2021-06-08 21:59:29 +0530

    initial commit closes #1

---
+++ src/main/java/com/hari/micro/springmicrobreweryclient/SpringMicrobreweryClientApplication.java
@@ -0,0 +1,13 @@
+package com.hari.micro.springmicrobreweryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicrobreweryClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicrobreweryClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hari/micro/springmicrobreweryclient/SpringMicrobreweryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hari.micro.springmicrobreweryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicrobreweryClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
